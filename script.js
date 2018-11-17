26b6f9ed6fd35674ffe57102db9429840edfd315
Chinmay <catrawalkar@gmail.com>
2018-11-18 00:40:31 +0530

    commit

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
+const randClassName = [
+    'gray-white',
+    'green-white',
+    'indigo-white',
+    'red-white',
+    'white-blue',
+    'white-gray',
+    'white-indigo',
+    'yellow-black',
+    'black-yellow',
+    'orange-black',
+    'black-orange'
+];
 $(document).ready(function () {
+    var randomNumber = Math.floor(Math.random() * randClassName.length);
+    $("main").addClass(randClassName[randomNumber]);
     $("body").click(function () {
-        var randClassName = [
-            'gray-white',
-            'green-white',
-            'indigo-white',
-            'red-white',
-            'white-blue',
-            'white-gray',
-            'white-indigo',
-            'yellow-black',
-            'black-yellow',
-            'orange-black',
-            'black-orange'
-        ];
-        var randomNumber = Math.floor(Math.random() * randClassName.length);
         $("main").removeClass();
+        var randomNumber = Math.floor(Math.random() * randClassName.length);
         $("main").addClass(randClassName[randomNumber]);
     });
 });
